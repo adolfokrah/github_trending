a0fa25ca9b1cec4f7da362a098fd7561ee4fe348
adolfokrah <adolfokrah@gmail.com>
2025-08-07 20:28:27 +0200

    fix: update

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@features": path.resolve(__dirname, "./src/features"),
     },
   },
 }));
